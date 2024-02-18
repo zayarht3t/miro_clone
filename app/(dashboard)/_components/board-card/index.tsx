941be7f1a0f9fee3e235fe1717b7072f2b357264
zayarht3t <zayy4699@gmail.com>
2024-02-18 13:51:00 +0630

    add favorite

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -6,6 +6,12 @@ import { Overlay } from "./overlay";
 import { useAuth } from "@clerk/nextjs"; 
 import { formatDistanceToNow } from "date-fns";
 import { Footer } from "./footer";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Actions } from "@/components/actions";
+import { MoreHorizontal } from "lucide-react";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 interface BoardCardProps {
     id: string;
@@ -33,6 +39,26 @@ export const BoardCard = ({
     const createdAtLabel = formatDistanceToNow(createdAt, {
         addSuffix: true
     })
+
+    const {
+        mutate: onFavorite,
+        pending: pendingFavorite
+    } = useApiMutation(api.board.favorite);
+
+    const {
+        mutate: onUnfavorite,
+        pending: pendingUnfavorite
+    } = useApiMutation(api.board.unfavorite);
+
+    const toggleFavorite = ()=>{
+        if(isFavorite){
+            onUnfavorite({id})
+                .catch(()=>toast.error("Failed to unfavorite"))
+        }else{
+            onFavorite({id,orgId})
+                .catch(()=>toast.error("Failed to unfavorite"))
+        }
+    }
     return(
         <Link href={`/board/${id}`}>
             <div className=" group aspect-[100/127] border rounded-lg flex
@@ -46,16 +72,38 @@ export const BoardCard = ({
                         className="object-fit"
                     />
                     <Overlay/>
+                    <Actions
+                        id={id}
+                        title={title}
+                        side="right"
+                    >
+                        <button
+                            className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none"
+                        >
+                            <MoreHorizontal
+                                className="text-white opacity-75 hover:opacity-100 transition-opacity"
+                            />
+                        </button>
+                    </Actions>
                 </div>
                 <Footer
                     isFavorite={isFavorite}
                     title={title}
                     authorLabel={authorLabel}
                     createdAtLabel={createdAtLabel}
-                    onClick={()=>{}}
-                    disabled={false}
+                    onClick={toggleFavorite}
+                    disabled={pendingFavorite  || pendingUnfavorite}
                 />
             </div>
         </Link>
+    )
+}
+
+BoardCard.Skeleton = function BoardCardSkeleton(){
+    return (
+        <div className="group aspect-[100/127] rounded-lg verflow-hidden">
+            <Skeleton className="h-full w-full"/>
+
+        </div>
     )
 }
--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -6,6 +6,7 @@ import { EmptyFavorite } from "./empty-favorites";
 import { EmptySearch } from "./empty-search";
 import { api } from "@/convex/_generated/api";
 import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./board-card/new-board-button";
 
 interface BoardListProps {
     orgId: string;
@@ -24,8 +25,19 @@ export const BoardList = ({
     if(data === undefined){
         return (
             <div>
-                Loading...
+            <h2 className="text-3xl">
+                {query.favorites ? "Favorite boards" : "Team boards"}
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4
+            lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10
+            ">
+                <NewBoardButton orgId={orgId} disabled/>
+                <BoardCard.Skeleton/>
+                <BoardCard.Skeleton/>
+                <BoardCard.Skeleton/>
+                <BoardCard.Skeleton/>
             </div>
+        </div>
         )
     }
 
@@ -54,6 +66,7 @@ export const BoardList = ({
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4
             lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10
             ">
+                <NewBoardButton orgId={orgId}/>
                 {
                     data?.map((board)=>(
                         <BoardCard
